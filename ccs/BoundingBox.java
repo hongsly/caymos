e3de4a20b790079e675a9309144d6588d35209bf
hongsly@gmail.com <hongsly@gmail.com@d4e90c75-78bc-548c-6009-3f4ef083a969>
2013-12-23 00:32:43 +0000

    git-svn-id: https://caymos.googlecode.com/svn/trunk@3 d4e90c75-78bc-548c-6009-3f4ef083a969

--- ccs/BoundingBox.java
+++ ccs/BoundingBox.java
@@ -1,3 +1,20 @@
+/*
+ This file is part of CayMos. 
+
+ CayMos is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ CayMos is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ccs;
 
 import ccs.graph.Graph;
@@ -6,12 +23,12 @@ import ccs.graph.Point2D;
 public class BoundingBox {
 
 	double x_min, x_max, y_min, y_max;
-	
-	public double getWidth(){
+
+	public double getWidth() {
 		return x_max - x_min;
 	}
-	
-	public double getHeight(){
+
+	public double getHeight() {
 		return y_max - y_min;
 	}
 
@@ -40,8 +57,8 @@ public class BoundingBox {
 				y_max = p.y();
 		}
 	}
-	
-	public String toString(){
-		return "["+x_min+","+x_max+";"+y_min+","+y_max+"]";
+
+	public String toString() {
+		return "[" + x_min + "," + x_max + ";" + y_min + "," + y_max + "]";
 	}
 }
